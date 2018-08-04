36b4dc670b91fc29fa5ae20f3d653279107ba067
Jonathan Mayunga <mayunga.j@gmail.com>
2018-08-04 02:15:20 +0200

    Update MBRateUs.swift

--- MBRateApp/Classes/MBRateUs.swift
+++ MBRateApp/Classes/MBRateUs.swift
@@ -11,9 +11,9 @@ import Foundation
 
 @objc open class MBRateUs: NSObject{
 
-    static let sharedInstance = MBRateUs()
+    open static let sharedInstance = MBRateUs()
 
-    public var rateUsInfo = MBRateUsInfo()
+    open var rateUsInfo = MBRateUsInfo()
     
     open func showRateUs(_ base:UIViewController, positiveBlock:@escaping ()->Void, negativeBlock:@escaping ()->Void, dismissBlock:@escaping ()->Void){
         let podBundle = Bundle(for: type(of: self))
@@ -29,8 +29,56 @@ import Foundation
         base.present(vc, animated: true, completion: nil)
     }
     
-    open func setTitle(_ title: String) {
-        self.rateUsInfo.title = title;
+    open func setTitle(_ title:String){
+        self.rateUsInfo.title = title
+    }
+    
+    open func setSubtitle(_ subtitle:String){
+        self.rateUsInfo.subtitle
+    }
+
+    open func setPositive(_ positive:String){
+        self.rateUsInfo.positive = positive
+    }
+    
+    open func setNegative(_ negative:String){
+        self.rateUsInfo.negative = negative
+    }
+    
+    open func setBackgroundColor(_ backgroundColor:UIColor){
+        self.rateUsInfo.backgroundColor = backgroundColor
+    }
+
+    open func setPositiveButtonColor(_ positiveButtonColor:UIColor){
+        self.rateUsInfo.positiveButtonColor = positiveButtonColor
+    }
+ 
+    open func setNegativeButtonColor(_ negativeButtonColor:UIColor){
+        self.rateUsInfo.negativeButtonColor = negativeButtonColor
+    }
+
+    open func setTextColor(_ textColor:UIColor){
+        self.rateUsInfo.textColor = textColor
+    }
+ 
+    open func setEmptyStarImage(_ emptyStarImage:UIImage?){
+        self.rateUsInfo.emptyStarImage = emptyStarImage
+    }
+    
+    open func setFullStarImage(_ fullStarImage:UIImage?){
+        self.rateUsInfo.fullStarImage = fullStarImage
+    }
+    
+    open func setTitleImage(_ titleImage:UIImage?){
+        self.rateUsInfo.titleImage = titleImage
+    }
+
+    open func setDismissButtonColor(_ dismissButtonColor:UIColor){
+        self.rateUsInfo.dismissButtonColor - dismissButtonColor
+    }
+
+    open func setItunesId(_ itunesId:String){
+        self.rateUsInfo.itunesId = itunesId
     }
     
 }
