238aef36da7ef65a9aed0a9c330c2cf4c622be15
Jonathan Mayunga <mayunga.j@gmail.com>
2018-08-27 12:37:50 +0200

    Update MBRateUsViewController.swift

--- MBRateApp/Classes/MBRateUsViewController.swift
+++ MBRateApp/Classes/MBRateUsViewController.swift
@@ -114,9 +114,9 @@ class MBRateUsViewController : UIViewController {
     }
     
     @IBAction func starTouched(_ sender: UIButton) {
-        /*self.starsMask.isHidden = false
+        self.starsMask.isHidden = false
         self.rating = sender.tag
-        if sender.tag >= 4 {
+        /*if sender.tag >= 4 {
             self.resultLabel.text = self.rateUsInfo?.positive
             self.callToActionButton.setTitle(self.rateUsInfo?.onPositiveButtonText, for: UIControlState())
             self.shouldRate = true
@@ -127,15 +127,15 @@ class MBRateUsViewController : UIViewController {
             self.callToActionButton.setTitle(self.rateUsInfo?.onNegativeButtonText, for: UIControlState())
             self.shouldRate = false
             self.callToActionButton.backgroundColor = self.rateUsInfo?.negativeButtonColor
-        }
+        }*/
         self.resultLabel.alpha = 0.0
         self.callToActionButton.alpha = 0.0
         self.resultLabel.isHidden = false
-        // self.callToActionButton.isHidden = false
+        self.callToActionButton.isHidden = false
         UIView.animate(withDuration: 0.5, animations: {() -> Void in
             self.resultLabel.alpha = 1.0
             self.callToActionButton.alpha = 1.0
-        }) */
+        }) 
     }
     
     @IBAction func callToActionTouched(_ sender: UIButton) {
